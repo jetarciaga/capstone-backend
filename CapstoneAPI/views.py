405c4360993fcaef6f08cece3596baaf2a14509c
Jethro Arciaga <jethro@openarchitectsk12.com>
2024-10-03 18:43:16 +0800

    Add ListUserAPIView with authentication

--- CapstoneAPI/views.py
+++ CapstoneAPI/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CustomUserSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class UserList(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = CustomUserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
