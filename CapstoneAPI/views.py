82dcf0ee81e999024f91d594f847ad2eb0a512b7
Jethro Arciaga <jet.arciaga@gmail.com>
2025-01-12 00:37:09 +0800

    fix dot notation for reference number

--- CapstoneAPI/views.py
+++ CapstoneAPI/views.py
@@ -224,7 +224,7 @@ class EmailView(APIView):
 
         requirements = self.extract_requirements(details.requirements)
         message = Template(email.message).substitute(
-            reference_number=details.reference_no,
+            reference_number=details.reference_number,
             user=details.user,
             status=details.status,
             document=document,
